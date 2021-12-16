7f9d61aa566a8a526998b1d65e0e10072c41854e
cha0sg0d <tony.goss@protonmail.com>
2021-12-16 15:28:27 -0500

    add method for sharing local RPC with testers

---
+++ tunnel.js
@@ -0,0 +1,20 @@
+const localtunnel = require('localtunnel');
+const opn = require('opn');
+
+
+(async () => {
+  const tunnel = await localtunnel({ port: 8545 });
+
+  // the assigned public url for your tunnel
+  // i.e. https://abcdefgjhij.localtunnel.me
+  // tunnel.url;
+
+  console.log(`\nDEV_PUBLIC_RPC=${tunnel.url}`);
+
+  // Opens url for user to approve.
+  opn(tunnel.url);
+
+  tunnel.on('close', () => {
+    // tunnels are closed
+  });
+})();
