f4d797efb0faae01fc559cf5c4662fec8a053146
cha0sg0d <tony.goss@protonmail.com>
2021-12-30 12:06:12 -0800

    add client to tunnel as well

--- tunnel.js
+++ tunnel.js
@@ -1,8 +1,7 @@
 const localtunnel = require('localtunnel');
 const opn = require('opn');
 
-
-(async () => {
+const jsonRPC = async () => {
   const tunnel = await localtunnel({ port: 8545 });
 
   // the assigned public url for your tunnel
@@ -17,4 +16,29 @@ const opn = require('opn');
   tunnel.on('close', () => {
     // tunnels are closed
   });
-})();
+};
+
+const client = async () => {
+  const tunnel = await localtunnel({ port: 8081, local_host: '127.0.0.1' });
+
+  // the assigned public url for your tunnel
+  // i.e. https://abcdefgjhij.localtunnel.me
+  // tunnel.url;
+
+  console.log(`\nDEV_PUBLIC_CLIENT=${tunnel.url}`);
+
+  // Opens url for deployer to approve.
+  opn(tunnel.url);
+
+  tunnel.on('close', () => {
+    // tunnels are closed
+  });
+};
+
+const main = () => {
+  // want sync calls
+  jsonRPC();
+  client();
+}
+
+main();
